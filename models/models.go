40c11b7933cea14f27687745569b721b4a894c89
Paras-tiket <106801571+Paras-tiket@users.noreply.github.com>
2022-07-06 08:44:37 +0530

    Add files via upload

---
+++ models/models.go
@@ -0,0 +1,29 @@
+package models
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+//Create Struct
+type Book struct {
+	ID     primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	SeatNo string             `json:"SeatNo" bson:"SeatNo,omitempty"`
+	User   *User              `json:"User" bson:"User,omitempty"`
+}
+
+type User struct {
+	FirstName string `json:"firstname,omitempty" bson:"firstname,omitempty"`
+	LastName  string `json:"lastname,omitempty" bson:"lastname,omitempty"`
+}
+type Train struct {
+	TrainID   string `json:"TrainID"`
+	TrainName string `json:"TrainName"`
+	CoachNo   string `json:"CoachNo"`
+	SeatNo    string `json:"SeatNo"`
+	TimeT     string `json:"TimeT"`
+	ClassT    string `json:"ClassT"`
+}
+
+type JsonResponse struct {
+	Type    string  `json:"type"`
+	Data    []Train `json:"data"`
+	Message string  `json:"message"`
+}
