e07180799dcffbc39c1e939dbf4a2ab620b963b0
Paras-tiket <106801571+Paras-tiket@users.noreply.github.com>
2022-07-06 13:28:38 +0530

    Update models.go

--- models/models.go
+++ models/models.go
@@ -6,6 +6,7 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 type Book struct {
 	ID     primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	SeatNo string             `json:"SeatNo" bson:"SeatNo,omitempty"`
+	TimeT string             `json:"TimeT" bson:"TimeT,omitempty"`
 	User   *User              `json:"User" bson:"User,omitempty"`
 }
 
